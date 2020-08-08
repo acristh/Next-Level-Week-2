cef0e740b1247e84322f02fa0f3b8a39a90019ea
André Araujo <andre.cristiano1975@gmail.com>
2020-08-08 13:49:01 +0000

    Initial commit

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Landing() {
+    return (
+        <div className="container">
+            <h2>Hello</h2>
+        </div>
+    );
+}
+
+export default Landing;
